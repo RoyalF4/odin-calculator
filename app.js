a5e1b7d3a732415df7fdaf3b8a4e9a13bf79591a
Royal Finch <Royal2791@gmail.com>
2024-01-25 17:36:10 -0500

    add functionality to operand, operator, and equals

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 let firstNumber;
 let secondNumber;
-let operator;
-let displayValue;
+let operator = '';
+let displayValue = '';
 let numberLength = 0;
 
 function add(num1, num2) {
-    return num1 + num2;
+    return String(Number(num1) + Number(num2));
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return String(Number(num1) - Number(num2));
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return String(Number(num1) * Number(num2));
 }
 
 function divide(num1, num2) {
-    return Number((num1 / num2).toFixed(10));
+    if(num2 == '0') return 'nice try';
+    return String(Number((num1 / num2).toFixed(10)));
 }
 
 function operate(operator, firstNumber, secondNumber) {
@@ -35,17 +36,21 @@ function operate(operator, firstNumber, secondNumber) {
 
 const buttons = document.querySelectorAll('.operand');
 const display = document.querySelector('#display');
+
+
+//  all clear button
 const clear = document.querySelector('#clear');
 
 clear.addEventListener('click', () => {
     displayValue = '';
     numberLength = 0;
-    firstNumber = 0;
-    secondNumber = 0;
-    operator = 0;
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
     display.textContent = '0';
 });
 
+// operand buttons
 buttons.forEach( button => {
     button.addEventListener('click', () => {
         if(numberLength !== 12) {
@@ -53,7 +58,50 @@ buttons.forEach( button => {
                 display.textContent = '';
             }
             display.textContent += button.value;
+            displayValue = display.textContent;
             numberLength++;
         }
     });
 });
+
+// event listeners for operator buttons
+const operators = document.querySelectorAll('.operator');
+
+operators.forEach( operatorBtn => {
+    operatorBtn.addEventListener('click', () => {
+        console.log(operatorBtn)
+            if(operator !== '') {
+                secondNumber = displayValue;
+                let result = operate(operator, firstNumber, secondNumber)
+                firstNumber = result;
+                operator = operatorBtn.value;
+                displayValue = '0';
+                display.textContent = `${result}`;
+                numberLength = 0;
+            }
+            else {
+                console.log(displayValue)
+                firstNumber = displayValue;
+                operator = operatorBtn.value;
+                displayValue = '0';
+                display.textContent = '0';
+                numberLength = 0;
+            }
+    })
+})
+
+// event listener for equals
+const equalsBtn = document.querySelector('.equals');
+
+equalsBtn.addEventListener('click', () => {
+    if(firstNumber !== '' && operator !== '') {
+        secondNumber = displayValue;
+        let result = operate(operator, firstNumber, secondNumber);
+        display.textContent = result;
+        firstNumber = result;
+        displayValue = result;
+        operator = '';
+        secondNumber = '';
+        numberLength = 0;
+    }
+})
