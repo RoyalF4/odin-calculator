078aaf40aa67277bd8dfeb97163a976157ae298f
Royal Finch <Royal2791@gmail.com>
2024-01-24 20:58:20 -0500

    add operate function

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -12,4 +17,17 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return Number((num1 / num2).toFixed(10));
+}
+
+function operate(operator, firstNumber, secondNumber) {
+    switch(operator) {
+        case '+':
+            return add(firstNumber,secondNumber);
+        case '-':
+            return subtract(firstNumber,secondNumber);
+        case '*':
+            return multiply(firstNumber,secondNumber);
+        case '/':
+            return divide(firstNumber,secondNumber);
+    }
 }
